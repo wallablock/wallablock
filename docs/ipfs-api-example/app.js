9c03b5aa101b1abdd35006447aff39568741cd12
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-03-30 12:59:52 +0200

    Minor API changes

--- docs/ipfs-api-example/app.js
+++ docs/ipfs-api-example/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var ipfs = require('wb-ipfs')
 var app = express();
+
 app.get('/', function (req, res) {
-    let ip = "";
-    let hash = "";
+    let ip = "79.147.40.189";
+    let hash = "QmP71gMJGWYtfRGRejq8ciNQkrrNTJA4mWZaaMstEL5fmR";
     res.setHeader('Content-Type', 'text/html');
     ipfs.IPFSread(ip,hash).then(links => {
         console.log(links);
