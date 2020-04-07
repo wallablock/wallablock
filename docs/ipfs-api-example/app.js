d8faaf7ce146a3f3001b57dc7450fc9273228d11
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-04-07 17:38:49 +0200

    Exemple escriptura

--- docs/ipfs-api-example/app.js
+++ docs/ipfs-api-example/app.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var ipfs = require('wb-ipfs')
+var ipfs = require('wb-ipfs');
 var app = express();
 
 app.get('/', function (req, res) {
-    let ip = "79.147.40.189";
-    let hash = "QmeqSB7T8bMGN84127daUtCouYEHRggEbnbMwcbnN5XiwQ";
+    let ip = "127.0.0.1";
+    
+    //Exemple lectura
     res.setHeader('Content-Type', 'text/html');
     ipfs.IPFSread(ip,hash).then(links => {
+        let hash = "";
         console.log(links);
         links.forEach((item, i) => {
             res.write(`<img src=${item}></img></br>`);
@@ -15,10 +17,16 @@ app.get('/', function (req, res) {
     }).catch(err => {
             console.log('Got error from IPFSread', err);
     });
-    /*let path = "";
-    let res = await ipfs.IPFSwrite(ip,path);
-    console.log(res);*/
+
+    //Exemple escriptura (funciona unicament si hi ha un node IPFS a localhost)
+    let path = "";
+    ipfs.IPFSwrite(ip,path).then(ret => {
+        console.log(ret);
+    }).catch(err => {
+        console.log(err)
+    });
 });
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
