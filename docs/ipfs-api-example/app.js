8554bb8cc41281491b97ae301b6bbb1dd967a47f
Thorg3l <44244742+Thorg3l@users.noreply.github.com>
2020-04-18 17:37:09 +0200

    Exemple de lectura adaptat a la reestructuració

--- docs/ipfs-api-example/app.js
+++ docs/ipfs-api-example/app.js
@@ -3,12 +3,12 @@ var ipfs = require('wb-ipfs');
 var app = express();
 
 app.get('/', function (req, res) {
-    let ip = "127.0.0.1";
-    let hash = "QmNrnUY9Fn9B3egTUHDdHQL366xfaPPXiD5KJ78EDRQdSZ"
+    let ip = "192.168.1.20";
+    let ipfsConnection = new ipfs.IpfsConnection(ip);
+    let hash = "QmcxAke8tG6cNJiZCqQoZvTE2yDoJu95VJ9KSQKYdkgcCm"
     //Exemple lectura
     res.setHeader('Content-Type', 'text/html');
-    ipfs.IPFSread(ip,hash).then(links => {
-        let hash = "";
+    ipfsConnection.read(hash).then(links => {
         console.log(links);
         links.forEach((item, i) => {
             res.write(`<img src=${item}></img></br>`);
@@ -19,12 +19,12 @@ app.get('/', function (req, res) {
     });
 
     //Exemple escriptura (funciona unicament si hi ha un node IPFS a localhost)
-    let path = "";
+    /*let path = "";
     ipfs.IPFSwrite(ip,path).then(ret => {
         console.log(ret);
     }).catch(err => {
         console.log(err)
-    });
+    });*/
 });
 
 app.listen(3000, function () {
