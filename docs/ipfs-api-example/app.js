c1761fbd69514d4a2f5659175c7b9600bdef7b67
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-05-01 20:00:40 +0200

    Format IPFS example

--- docs/ipfs-api-example/app.js
+++ docs/ipfs-api-example/app.js
@@ -10,53 +10,53 @@ let app = express();
 let ipfsConnection = new ipfs.IpfsConnection(ipfsNodeIp);
 const hash = "QmPwn2M19Li41k6v8r4WnERiiPFLw93mceGaafMbSjduF8";
 
-app.get("/readCover", async function(_req, res, next) {
-    let link;
-    try {
-        link = await ipfsConnection.coverUrl(hash);
-    } catch (err) {
-        console.error("Error from readCover:", err);
-        return next(`Error: ${err}`);
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write(`<img src=${link}></img><br />`);
-    res.end();
+app.get("/readCover", async function (_req, res, next) {
+  let link;
+  try {
+    link = await ipfsConnection.coverUrl(hash);
+  } catch (err) {
+    console.error("Error from readCover:", err);
+    return next(`Error: ${err}`);
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<img src=${link}></img><br />`);
+  res.end();
 });
 
-app.get("/readDesc", async function(_req, res, next) {
-    let desc;
-    try {
-        desc = await ipfsConnection.fetchDesc(hash);
-    } catch (err) {
-        console.error("Error from fetchCover:", err);
-        return next(`Error: ${err}`);
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write(`<p1>${desc}</p1>`);
-    res.end();
+app.get("/readDesc", async function (_req, res, next) {
+  let desc;
+  try {
+    desc = await ipfsConnection.fetchDesc(hash);
+  } catch (err) {
+    console.error("Error from fetchCover:", err);
+    return next(`Error: ${err}`);
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<p1>${desc}</p1>`);
+  res.end();
 });
 
-app.get("/readImages", async function(_req, res, next) {
-    // Read Example
-    let links;
-    try {
-        links = await ipfsConnection.getAllImagesUrl(hash);
-    } catch (err) {
-        console.error("Error from getAllImagesUrl:", err);
-        // Passes error to Express so it can return it with a 500 Server Error status
-        return next(`Error: ${err}`);
-    }
-    res.setHeader("Content-Type", "text/html");
-    for (let item of links) {
-        res.write(`<img src=${item}></img><br />`);
-    }
-    res.end();
+app.get("/readImages", async function (_req, res, next) {
+  // Read Example
+  let links;
+  try {
+    links = await ipfsConnection.getAllImagesUrl(hash);
+  } catch (err) {
+    console.error("Error from getAllImagesUrl:", err);
+    // Passes error to Express so it can return it with a 500 Server Error status
+    return next(`Error: ${err}`);
+  }
+  res.setHeader("Content-Type", "text/html");
+  for (let item of links) {
+    res.write(`<img src=${item}></img><br />`);
+  }
+  res.end();
 });
 
-app.get("/write", async function(_req, res, next) {
-    console.log("funciona");
-    res.setHeader("Content-Type", "text/html");
-    res.write(`<html>
+app.get("/write", async function (_req, res, next) {
+  console.log("funciona");
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html>
         <body>
         <form action="${ipfsNodeIp}/wb/upload" method="post" enctype="multipart/form-data">
           <input type="file" id="files" name="files" multiple><br><br>
@@ -64,9 +64,9 @@ app.get("/write", async function(_req, res, next) {
         </form>
         </body>
         </html>`);
-    res.end();
+  res.end();
 });
 
 app.listen(3030, function () {
-    console.log('Example app listening on port 3030!');
+  console.log("Example app listening on port 3030!");
 });
